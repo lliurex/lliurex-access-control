dfaf385ead47d607deb18138358917f3b88d26bd
Juan Pelegrina <juapesai@hotmail.com>
2022-02-15 18:03:49 +0100

    Added po files

---
+++ lliurex-po/generate_po.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+xgettext -kde -ki18nd:2 ../lliurex-access-control.install/ui/rsrc/lliurex-access-control.qml -o ../lliurex-po/lliurex-access-control/lliurex-access-control.pot
+xgettext --join-existing -kde -ki18nd:2 ../lliurex-access-control.install/ui/rsrc/Login.qml -o ../lliurex-po/lliurex-access-control/lliurex-access-control.pot
+xgettext --join-existing -kde -ki18nd:2 ../lliurex-access-control.install/ui/rsrc/ApplicationOptions.qml -o ../lliurex-po/lliurex-access-control/lliurex-access-control.pot
+xgettext --join-existing -kde -ki18nd:2 ../lliurex-access-control.install/ui/rsrc/GroupsSettings.qml -o ../lliurex-po/lliurex-access-control/lliurex-access-control.pot
+xgettext --join-existing -kde -ki18nd:2 ../lliurex-access-control.install/ui/rsrc/ListDelegateGroupItem.qml -o ../lliurex-po/lliurex-access-control/lliurex-access-control.pot
+xgettext --join-existing -kde -ki18nd:2 ../lliurex-access-control.install/ui/rsrc/UsersSettings.qml -o ../lliurex-po/lliurex-access-control/lliurex-access-control.pot
+xgettext --join-existing -kde -ki18nd:2 ../lliurex-access-control.install/ui/rsrc/ListDelegateUserItem.qml -o ../lliurex-po/lliurex-access-control/lliurex-access-control.pot
+xgettext --join-existing -kde -ki18nd:2 ../lliurex-access-control.install/ui/rsrc/ChangesDialog.qml -o ../lliurex-po/lliurex-access-control/lliurex-access-control.pot
+
