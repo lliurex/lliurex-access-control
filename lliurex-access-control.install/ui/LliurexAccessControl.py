a693f232d28d0caa39fddbd4e3cb1b41aac47f6d
jrpelegrina <juapesai@hotmail.com>
2022-02-16 11:59:12 +0100

    Fixed get lang to show help

--- lliurex-access-control.install/ui/LliurexAccessControl.py
+++ lliurex-access-control.install/ui/LliurexAccessControl.py
@@ -680,9 +680,8 @@ class LliurexAccessControl(QObject):
 
 	@Slot()
 	def openHelp(self):
-		lang=os.environ["LANG"]
 		
-		if 'valencia' in lang:
+		if 'valencia' in self.n4dMan.sessionLang:
 			self.help_cmd='xdg-open https://wiki.edu.gva.es/lliurex/tiki-index.php?page=Lliurex-Access-Control.'
 		else:
 			self.help_cmd='xdg-open https://wiki.edu.gva.es/lliurex/tiki-index.php?page=Lliurex-Access-Control'
